972daf65d6f0e3f57e4393bd8a49d3e7c9193b14
Marlon Calvo <marloncalv98@gmail.com>
2019-10-21 15:27:32 -0400

    first publish

---
+++ index.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+var zmq = require('zeromq');
+var responder = zmq.socket('pull');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  responder.on('message', function(request) {
+    console.log(request.toString());
+    socket.emit('post', request.toString());
+  });
+  socket.on('request', function(msg){
+    console.log(msg);
+    socket.emit('post', msg);
+  });
+});
+
+responder.bind('tcp://*:5555', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Listening on 5555...");
+  }
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+process.on('SIGINT', function() {
+  responder.close();
+});
+
