11d8a758a2f931e01cb33f106648c9159a083ac9
Marlon Calvo <marloncalv98@gmail.com>
2019-10-28 18:10:59 -0400

    add vagrant setup to project

--- server/index.js
+++ server/index.js
@@ -1,44 +1,25 @@
-
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
-var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-
-var zmq = require('zeromq');
-var responder = zmq.socket('pull');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.text({ type: 'text/*' }))
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  responder.on('message', function(request) {
-    console.log(request.toString());
-    socket.emit('post', request.toString());
-  });
-  socket.on('request', function(msg){
-    console.log(msg);
-    socket.emit('post', msg);
+  console.log('connected!')
+  app.post('/new', function(req, res){
+    res.end()
+	  console.log('received ', req.body)
+    socket.emit('post', req.body)
   });
 });
 
-responder.bind('tcp://*:5555', function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Listening on 5555...");
-  }
+http.listen(3000, function(){
+  console.log('listening on *:' + 3000);
 });
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
-
-process.on('SIGINT', function() {
-  responder.close();
-});
-
---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add repo for postgresql latest versions.
+# Fix for NO_PUBKEY issue.
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7FCC7D46ACCC4CF8
+add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+apt-get update
+apt-get install -y postgresql postgresql-contrib build-essential
+
+# This installs LTS version of Node.js v12.
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
+
+# Setup up enviroment.
+ln -s /shared/code/ /home/vagrant/code
