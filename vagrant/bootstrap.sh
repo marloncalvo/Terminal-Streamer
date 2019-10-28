f5d305af1e936d0d14f4beb2be9a7461843aa07b
vagrant <vagrant@vagrant.vm>
2019-10-28 22:28:14 +0000

    add python2.7 as dependency, as well as yarn.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,11 +7,12 @@ add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg mai
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 apt-get update
-apt-get install -y postgresql postgresql-contrib build-essential
+apt-get install -y "postgresql" "postgresql-contrib" "build-essential" "python2.7"
 
 # This installs LTS version of Node.js v12.
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs
+npm install -g yarn
 
 # Setup up enviroment.
 ln -s /shared/code/ /home/vagrant/code
